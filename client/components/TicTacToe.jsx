9bfe2411fb110a1730e4126fc027214e3e1d14a6
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-08-09 00:14:32 +1200

    added onClick cylces both moves / useEffect to clear board on page load

--- client/components/TicTacToe.jsx
+++ client/components/TicTacToe.jsx
@@ -1,23 +1,63 @@
-import React from 'react'
+import React, {  useState, useEffect } from 'react'
 import { Route, Link } from 'react-router-dom'
 
 
 const TicTacToe = () => {
+
+    const [tic, setTic] = useState('O')
+    let palyOptions = ['O', 'X']
+
+ //Clears board on page load
+ useEffect(() => {
+    setTic('')
+  },[])
+
+// Handles player click - still testing final solution - currently both moves onClick
+ const handleClick = () => {
+    
+    if ( tic === '') {
+        setTic(palyOptions[0])
+    } else  if (tic === 'O'){    
+        setTic(palyOptions[1])
+    } else {
+        setTic(palyOptions[0])
+    }
+ }
+
+
 
     return(
         <div className='ticContainer'>
             <h1>TicTacToe</h1>
 
                 <div className='ticGameContainer'>
-                    <div className='ticCell'></div>
-                    <div className='ticCell'></div>
-                    <div className='ticCell'></div>
-                    <div className='ticCell'></div>
-                    <div className='ticCell'></div>
-                    <div className='ticCell'></div>
-                    <div className='ticCell'></div>
-                    <div className='ticCell'></div>
-                    <div className='ticCell'></div>
+                    <div className='ticCell' onClick={handleClick}>
+                        <h3 className='ticValue'>{tic}</h3>
+                    </div>
+                    <div className='ticCell' onClick={handleClick}>
+                        <h3 className='ticValue'>{tic}</h3>
+                    </div>
+                    <div className='ticCell' onClick={handleClick}>
+                        <h3 className='ticValue'>{tic}</h3>
+                    </div>
+                    <div className='ticCell' onClick={handleClick}>
+                        <h3 className='ticValue'>{tic}</h3>
+                    </div>
+                    <div className='ticCell' onClick={handleClick}>
+                        <h3 className='ticValue'>{tic}</h3>
+                    </div>
+                    <div className='ticCell' onClick={handleClick}>
+                        <h3 className='ticValue'>{tic}</h3>
+                    </div>
+                    <div className='ticCell' onClick={handleClick}>
+                        <h3 className='ticValue'>{tic}</h3>
+                    </div>
+                    <div className='ticCell' onClick={handleClick}>
+                        <h3 className='ticValue'>{tic}</h3>
+                    </div>
+                    <div className='ticCell' onClick={handleClick}>
+                        <h3 className='ticValue'>{tic}</h3>
+                    </div>
                 </div>
 
 
